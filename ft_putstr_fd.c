4ea2473afd41c523ef69bde9e98f6592631f7059
rober <101320752+roberw21313331we@users.noreply.github.com>
2024-10-04 21:53:34 +0200

    falta probar el iteri y el header en todos los fd mas completar el putnbr

---
+++ ft_putstr_fd.c
@@ -0,0 +1,7 @@
+void ft_putstr_fd(char *s, int fd)
+{
+    while (*s)
+    {
+        write(fd,*s,1);
+    }
+}
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -21,7 +21,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	str = (char *)malloc(ft_strlen(s) + 1);
 	if (!str)
 		return (NULL);
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
 		str[i] = f(i, s[i]);
 		i++;
