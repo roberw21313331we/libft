8d4c4effcadfd0cfe8fbca5f786cef6092fd63a3
rober <101320752+roberw21313331we@users.noreply.github.com>
2024-10-10 21:06:26 +0200

    esta todo bien solo faltan los bonus

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcarpio- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 19:37:32 by rcarpio-          #+#    #+#             */
+/*   Updated: 2024/10/10 19:37:49 by rcarpio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*n;
+
+	n = (t_list *)malloc(sizeof(t_list));
+	if (!n)
+		return (NULL);
+	n->content = content;
+	n->next = NULL;
+	return (n);
+}
